a26520daf2c58b4d07678295ef23ebb64e9ca564
luoshaoyinn <luoshaoyin641@outlook.com>
2025-08-06 21:48:44 +0800

    Super variable ok

---
+++ super_variable/super_variable.c
@@ -0,0 +1,159 @@
+#include "super_variable.h"
+
+static inline int
+acquire_meta_lock(super_variable p)
+{
+    int result = pthread_mutex_lock(&p->mtx);
+    if (result == EOWNERDEAD) {
+        // The mutex is now locked, but the state needs to be reset
+        pthread_mutex_consistent(&p->mtx);
+        // After this, it's safe to use the mutex again
+    } else if (result != 0) {
+        return result;
+    }
+    return 0;
+}
+
+
+static inline int
+release_meta_lock(super_variable p)
+{
+    pthread_mutex_unlock(&p->mtx);
+    return 0;
+}
+
+
+static inline size_t
+get_full_size(size_t size)
+{
+    return sizeof(struct _s_super_variable) + size * CIRCLE_QUEUE_LENGTH;
+}
+
+
+static inline bool
+is_later_than(const struct timespec a, const struct timespec b) {
+    return a.tv_sec > b.tv_sec || \
+        (a.tv_sec == b.tv_sec && a.tv_nsec > b.tv_nsec);
+}
+
+
+super_variable
+link_super_variable(const char *name, size_t size)
+{
+    /* Try to open an existing and create if failed */
+    int fd = shm_open(name, O_CREAT | O_EXCL | O_RDWR, 0600);
+    bool is_create = true;
+    if(fd == -1) {
+        if(errno == EEXIST) {
+            fd = shm_open(name, O_RDWR, 0600);
+            is_create = false;
+        }
+        if(fd == -1)   // other error 
+            goto END;
+    }
+
+    /* Change the size */
+    if(ftruncate(fd, get_full_size(size)) == -1)
+        goto END;
+    
+    /* Map it into process's memory */
+    super_variable p = (super_variable)mmap(NULL, \
+            get_full_size(size), \
+            PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(p == MAP_FAILED) goto END;
+    
+    /* Initialize the mutex if created */
+    if(is_create) { 
+        // Prepare for the attribute of the mutex lock
+        pthread_mutexattr_t attr;
+        pthread_mutexattr_init(&attr);
+        pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+        pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST);
+        pthread_mutex_init(&p->mtx, &attr);
+        pthread_mutexattr_destroy(&attr);
+    }
+
+    return p;
+END:
+    munmap(p, size);
+    shm_unlink(name);
+    return NULL;
+}
+
+
+void
+unlink_super_variable(super_variable p, const char *name, size_t size)
+{
+    munmap(p, get_full_size(size));
+    shm_unlink(name);
+}
+
+
+int
+read_super_variable(super_variable p, void *buf, size_t size)
+{
+    int err_code = 0;
+
+    /* Modify to the meta block should be very fast */
+    err_code = acquire_meta_lock(p);
+    if(err_code) return err_code;
+    uint8_t qh = p->qhead, qt = p->qtail;
+    while(qh != qt && \
+            memcmp(&p->b_timestamp[qh], &p->timestamp, sizeof(struct timespec)))
+        qh = ((qh + 1) & (CIRCLE_QUEUE_LENGTH - 1)); 
+    p->qhead = qh; p->qtail = qt; 
+    release_meta_lock(p);
+    
+    /* mem copying may be costly */
+    if(qh == qt) return -1; // Nothing to read
+    memcpy( buf, \
+            p->data + qh * size, \
+            size);
+    
+    return 0;
+}
+
+
+int
+write_super_variable(super_variable p, void *data, size_t size)
+{
+    int err_code = 0;
+    uint8_t tmp;
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+
+    /* Try to get a free buffer */
+    err_code = acquire_meta_lock(p);
+    if(err_code) // We haven't acquire a lock here, so just return is ok
+        return err_code;
+    uint8_t qh = p->qhead, qt = p->qtail;
+    if(is_later_than(p->timestamp, ts)) { // Just do nothing if not the lastest 
+        err_code = 1;
+        goto END_METALOCK;
+    }
+    while(qh != qt && \
+            memcmp(&p->b_timestamp[qh], &p->timestamp, sizeof(struct timespec)))
+        qh = ((qh + 1) & (CIRCLE_QUEUE_LENGTH - 1)); 
+
+    /* Append the selected buffer to the end of the queue */
+    tmp = qt; // [qh, qt), so we have to write to tmp
+    qt = ((qt + 1) & (CIRCLE_QUEUE_LENGTH - 1));
+    if(qh == qt) {  // the circle queue is full
+        qt = tmp; // roll back
+        err_code = -1;
+        goto END_METALOCK;
+    }
+    p->b_timestamp[tmp] = p->timestamp = ts;
+
+    /* Don't forget to write into the metablock and unlock */
+END_METALOCK: 
+    p->qtail = qt; p->qhead = qh;
+    release_meta_lock(p);
+    if(err_code) return err_code;
+
+    /* mem copying */
+    memcpy( p->data + tmp * size, \
+            data, \
+            size);
+    return 0;
+}
---
+++ super_variable/super_variable.h
@@ -0,0 +1,37 @@
+/* super_variable.h
+ */
+
+#ifndef _H_SUPER_VARIABLE
+
+#include <time.h>
+#include <fcntl.h> 
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#define CIRCLE_QUEUE_LENGTH 4  /* must be the power of 2 */
+
+struct _s_super_variable {
+    pthread_mutex_t mtx;
+    uint8_t qhead, qtail;
+    struct timespec timestamp;
+    struct timespec b_timestamp[CIRCLE_QUEUE_LENGTH];
+    uint8_t data[];
+};
+
+typedef struct _s_super_variable* super_variable;
+
+super_variable link_super_variable(const char *name, size_t size);
+int read_super_variable(super_variable p, void *buf, size_t size);
+int write_super_variable(super_variable p, void *data, size_t size);
+void unlink_super_variable(super_variable p, const char *name, size_t size);
+
+#endif
