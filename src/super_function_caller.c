7a6c5b01e60739fc74b082d8e64f4791cd5ccb82
wegg111 <1047950878@qq.com>
2025-08-14 22:52:04 +0800

    add error code in pipe

--- src/super_function_caller.c
+++ src/super_function_caller.c
@@ -9,41 +9,48 @@
 #include <sys/stat.h>
 
 #include "super_function_caller.h"
+#include "constant.h"
 
 struct _s_super_function_caller {
     int req_id, res_id;
 };
 
 
-int static inline
-_try_to_open_pipe(const char *name)
+int static inline _try_to_open_pipe(const char *name)
 {
     /* Try to create first, then open it if already exists */
     if (mkfifo(name, 0600) == -1) {
         if (errno != EEXIST) /* other error rather than EEXIST */
-            return -1;
+            return ERR_PIPE_REQ;
     }
 
-    /* Then open it */
+    /* Then open it, return ID */
     return open(name, O_RDWR);
 }
 
 
-super_function_caller
-link_super_function(const char *name) 
+super_function_caller link_super_function(const char *name) 
 {
     static char name_buf[256];
     super_function_caller p = malloc(sizeof(struct _s_super_function_caller));
     
-    /* connect to the request pipe */
-    sprintf(name_buf, "/tmp/THMOS/%s_req", name);
+    /* connect to the request pipe: /tmp/robot_icp/{name}_req */
+    sprintf(name_buf, "/tmp/robot_icp_pipe/%s_req", name);
     p->req_id = _try_to_open_pipe(name_buf);
 
-    /* connect to the response pipe */
+    /* indicate a failed operation */
+    if(p->req_id == ERR_PIPE_REQ) { 
+        free(p);
+        return NULL;
+    }
+
+    /* connect to the response pipe: {name}_res */
     sprintf(name_buf, "%s_res", name);
     p->res_id = _try_to_open_pipe(name_buf);
 
-    if(p->req_id == -1 || p->res_id == -1) { /* indicate a failed operation */
+
+    if(p->res_id == ERR_PIPE_REQ) {
+        close(p->req_id);
         free(p);
         return NULL;
     }
@@ -52,29 +59,35 @@ link_super_function(const char *name)
 }
 
 
-int
-unlink_super_function(super_function_caller p)
+int unlink_super_function(super_function_caller p)
 {
     int ret = 0;
+
     ret = close(p->req_id);
-    if(ret) return ret;
+    if(ret) 
+        return ERR_PIPE_CLOSE;
+
     ret = close(p->res_id);
-    if(ret) return ret;
+    if(ret) 
+        return ERR_PIPE_CLOSE;
+
     free(p);
-    return 0;
+    return ROBOT_OK;
 }
 
 
-ssize_t 
-call_super_function(super_function_caller p,
+ssize_t call_super_function(super_function_caller p,
         void *args, size_t args_sz, \
         void *ret_buf)
 {
     write(p->req_id, &args_sz, sizeof(size_t));
     write(p->req_id, args, args_sz);
+
     static ssize_t ret;
+
     read(p->res_id, &ret, sizeof(size_t));
     read(p->res_id, ret_buf, ret);
+    
     return ret;
 }
 
